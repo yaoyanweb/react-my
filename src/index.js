bb1add2e285643dc1f19eef5d9b4481a0738d59e
yaoyan <yaoyan@souche.com>
2020-08-14 18:13:10 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-31 07:29:20
- * @LastEditTime: 2020-08-13 19:16:19
+ * @LastEditTime: 2020-08-13 20:06:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /react-my/src/index.js
@@ -10,14 +10,34 @@
 import React from 'react'; //核心库
 import ReactDOM from 'react-dom';
 import './index.css';  // DOM渲染库
-
+class MouseTracker extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {x:0,y:0};
+    }
+    handleMove =(e) => {
+      this.setState({
+        x:e.clientX,
+        y: e.clientY
+      })
+    }
+    render(){
+      return (
+        <div onMouseMove = {this.handleMove}>
+        
+        </div>
+      )
+    }
+}
 
 
 ReactDOM.render(
-  <>
-  
-  
-  </>
+<MouseTracker>
+  <h1>{this.props.children}</h1>
+  <h1>移动鼠标</h1>
+  <p>当前坐标是{this.state.x}, {this.state.y}</p>
+
+</MouseTracker>
 ,
   document.getElementById('root')
 );
