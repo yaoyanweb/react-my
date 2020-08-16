0ee8813237ceb2189a3694d2447dc6a77d8d3b07
yaoyan <yaoyan@souche.com>
2020-08-17 07:50:35 +0800

    高阶组件借宿

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-31 07:29:20
- * @LastEditTime: 2020-08-13 20:06:08
+ * @LastEditTime: 2020-08-17 07:47:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /react-my/src/index.js
@@ -24,20 +24,64 @@ class MouseTracker extends React.Component {
     render(){
       return (
         <div onMouseMove = {this.handleMove}>
-        
+          {this.props.render(this.state)}
         </div>
       )
     }
 }
 
+function withMouseTracker(OldComponent){
+  return (props)=> <MouseTracker render={
+    value => <OldComponent {...props} {...value} />
+  } />
+}
+
+let NewMouseTracker = withMouseTracker(props => (
+  <>
+   <h1>{props.title}</h1> 
+   <h1>移动鼠标</h1>
+   <p>当前坐标是{props.x}, {props.y}</p>
+  </>
+));
 
 ReactDOM.render(
+  <NewMouseTracker title="follow"  />,document.getElementById('root')
+);
+
+// 第二种 通过render
+
+/* ReactDOM.render(
+  <MouseTracker render={
+    props => (
+      <>
+       <h1>移动鼠标</h1>
+       <p>当前坐标是{props.x}, {props.y}</p>
+      </>
+    )
+  } />
+  ,
+    document.getElementById('root')
+  );
+ */
+
+
+
+
+// 第一种 组件的儿子 children是一个函数
+/* ReactDOM.render(
 <MouseTracker>
-  <h1>{this.props.children}</h1>
-  <h1>移动鼠标</h1>
-  <p>当前坐标是{this.state.x}, {this.state.y}</p>
+ 
+ {
+   props => (
+     <>
+      <h1>移动鼠标</h1>
+      <p>当前坐标是{props.x}, {props.y}</p>
+     </>
+   )
+ }
 
 </MouseTracker>
 ,
   document.getElementById('root')
 );
+ */
